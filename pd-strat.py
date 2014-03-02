348bf1a3c32d7ab60f8d048aa0241552228c4307
Patrick Douglas <pcdouglas94@gmail.com>
2014-03-01 18:21:16 -0800

    First working strategy and rename of brainstorming file.

--- pd-strat.py
+++ pd-strat.py
@@ -1,10 +1,32 @@
-#Patrick Douglas Strategy/AI
+#Patrick Douglas 
+#Robot Strategy/AI
 import rg
+
+"""
+-check if in spawn zone. 
+   if yes, find open space and go there.
+   if no, continue
+
+-check surrounding area.
+    if 1 enemy:
+      if myhealth > enemyhealth:
+        attack
+      if myhealth < enemyhealth:
+         if they have less than 15, suicide
+         else back away
+    if 2 or more enemy:
+      if both less than 15 and mine less than 20, suicide
+      else back away
+    else go to move
+
+-move to center:
+  if cant go further, guard
+"""
 
 class Robot:
     def act(self, game):
         # Determine what to do:
-        ret_value = []
+        ret_value = ['empty']
         ret_value = self.spawn(game, ret_value)
         ret_value = self.attack(game, ret_value)
         ret_value = self.otherwise(game, ret_value)
@@ -12,25 +34,66 @@ class Robot:
     
     #Checks to see if player is in spawn.
     def spawn(self, game, current_return):
-        #CHECK IF RETURN IS SET
+        if current_return != ['empty']:
+            return current_return
+        #If bot is in spawn, determine how to move out.
         if rg.loc_types(self.location) == 'spawn':
             possible_locations = rg.locs_around(self.location, filter_out = ('invalid', 'obstacle', 'spawn'))
+            enemy_locs = []
             for locs in possible_locations:
-                #if game.robots[locs].player_id != self.player_id:
-                #If health low, attack it, if high, remove it.
-                pass
+                if locs in game.robots and game.robots[locs].player_id != self.player_id:
+                    enemy_locs.append(locs)
+            for enemy in enemy_locs:
+                if enemy.hp < 9:
+                    return ['attack', enemy]
+                elif enemy in possible_locations:
+                    possible_locations.remove(enemy)
             best_option = [100, (0,0)]
             for locs in possible_locations:
                 current = [rg.wdist(locs, rg.CENTER_POINT), locs]
                 if current[0] <= best_option[0]:
                     best_option = current
             return ['move', best_option[1]]
+        return current_return
 
     #Will check for nearby enemies and respond appropriately.
     def attack(self, game, current_return):
-        pass
+        if current_return != ['empty']:
+            return current_return
+        possible_locations = rg.locs_around(self.location, filter_out = ('invalid', 'obstacle', 'spawn'))
+        enemy_locs = []
+        enemy_num = 0
+        for locs in possible_locations:
+            if locs in game.robots and game.robots[locs].player_id != self.player_id:
+                enemy_num += 1
+                enemy_locs.append(locs)
+        #Once we have a list of enemies, we can follow a process to determine how to deal with them.
+        """ if 1 enemy:
+            if myhealth > enemyhealth:
+            attack
+            if myhealth < enemyhealth:
+            if they have less than 15, suicide
+            else back away
+            if 2 or more enemy:
+            if both less than 15 and mine less than 20, suicide
+            else back away
+            else go to move """
+
+        for enemy in enemy_locs:
+            return ['attack', enemy]
+
+        #Need to flesh out attack strategy
+            if enemy_num == 1:
+                return ['attack', enemy]
+        return current_return
 
     #Guaranteed to return something if nothing else is returned.
     def otherwise(self, game, current_return):
-        return ['guard']
+        if current_return != ['empty']:
+            return current_return
+        if self.location != rg.CENTER_POINT:
+            towards_center = rg.toward(self.location, rg.CENTER_POINT)
+            return ['move', towards_center]
+        else:
+            return ['guard']
 
